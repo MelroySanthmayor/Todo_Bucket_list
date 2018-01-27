f4b192df0aa4f67306571471df31b1800cbe8780
MelroySanthmayor <melsanthmayor87@gmail.com>
2018-01-27 16:40:28 +0530

    updated  __init__.py file

--- todo/__init__.py
+++ todo/__init__.py
@@ -1,12 +1,12 @@
 import os
 from flask import Flask
-from flask.ext.cors import CORS
+from flask_cors import CORS
 
-DATABASE = os.getenv("DATABASE_URL", "sqlite:///debug.db")
+DATABASE = os.getenv("DATABASE_URL", "postgresql://postgres:mlry871995@localhost/flaskmovie")
 DATABASE = DATABASE.strip()
 
 app = Flask(__name__)
 app.config.from_object(__name__)
-CORS(app, resources=r'/*', allow_headers="Content-Type")
+CORS(app, resources=r'/sqlalchemy/*', allow_headers="Content-Type")
 
 import todo.views
